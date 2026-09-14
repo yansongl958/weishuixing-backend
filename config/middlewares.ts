d29a48ab72647957feee579913918bc94d35a901
yansongl958 <yansongl@gmail.com>
2026-09-14 12:57:59 +0800

    修复Railway环境下登录时secure cookie报错

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,7 +8,12 @@ const config: Core.Config.Middlewares = [
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+    {
+    name: 'strapi::session',
+    config: {
+      proxy: true,
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
